8a5a138f1741d277e32e2caf14d1de2429662c62
Luqman Olushi <olushilukmon03@gmail.com>
2025-02-01 15:58:13 +0100

    feat: remove/hide ad banners

--- src/contentScript.ts
+++ src/contentScript.ts
@@ -1,10 +1,9 @@
 const style = document.createElement("style");
 
 style.textContent = `
-  ytd-ad-slot-renderer {
+  ytd-ad-slot-renderer, .ytd-player-legacy-desktop-watch-ads-renderer, ytd-companion-slot-renderer, [target-id="engagement-panel-ads"] {
     display: none !important;
   }
-
 `;
 
 document.head.appendChild(style);
