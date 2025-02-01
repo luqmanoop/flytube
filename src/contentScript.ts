c6cd8db989b6f11562dba194319d23eab2c841ae
Luqman Olushi <olushilukmon03@gmail.com>
2025-02-01 16:20:41 +0100

    feat: add overlay on video player

--- src/contentScript.ts
+++ src/contentScript.ts
@@ -1,3 +1,4 @@
+import { waitForElement } from "./utils";
 const style = document.createElement("style");
 
 style.textContent = `
@@ -7,3 +8,40 @@ style.textContent = `
 `;
 
 document.head.appendChild(style);
+
+// const iframe = document.createElement("iframe");
+
+const isVideoUrl = (url: string) => /youtube.com\/watch\?v=/.test(url);
+
+const getVideoId = (url: string) => url.split("/watch?v=")[1];
+
+const getActiveVideo = async () =>
+	waitForElement("#player #container.ytd-player");
+
+const renderOverlayOnVideo = (activeVideo: HTMLElement) => {
+	const overlay = document.createElement("div");
+	overlay.id = "overlay";
+	overlay.style.cssText = `
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    background-color: rgba(0, 0, 0, 0.5);
+    z-index: 10;
+  `;
+	overlay.style.pointerEvents = "none";
+
+	activeVideo.style.position = "relative";
+	activeVideo.appendChild(overlay);
+
+	return overlay;
+};
+
+(async () => {
+	setTimeout(async () => {
+		const activeVideo = await getActiveVideo();
+		if (!activeVideo) return;
+		renderOverlayOnVideo(activeVideo);
+	}, 4000);
+})();
