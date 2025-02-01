8a5765e47b318bf04163bda01d27311cf59be61d
Luqman Olushi <olushilukmon03@gmail.com>
2025-02-01 15:30:36 +0100

    feat: remove/hide ad slots

--- src/contentScript.ts
+++ src/contentScript.ts
@@ -1,1 +1,10 @@
-console.log("content script loaded");
+const style = document.createElement("style");
+
+style.textContent = `
+  ytd-ad-slot-renderer {
+    display: none !important;
+  }
+
+`;
+
+document.head.appendChild(style);
