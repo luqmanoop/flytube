9cc2766d895ae4258de1799a624dceec8c072146
Luqman Olushi <olushilukmon03@gmail.com>
2025-02-02 07:23:48 +0100

    feat: load overlay player

--- src/contentScript.ts
+++ src/contentScript.ts
@@ -1,4 +1,4 @@
-import { waitForElement } from "./utils";
+import { waitFor, waitForElement } from "./utils";
 const style = document.createElement("style");
 
 style.textContent = `
@@ -9,39 +9,60 @@ style.textContent = `
 
 document.head.appendChild(style);
 
-// const iframe = document.createElement("iframe");
-
-const isVideoUrl = (url: string) => /youtube.com\/watch\?v=/.test(url);
+const iframe = document.createElement("iframe");
 
 const getVideoId = (url: string) => url.split("/watch?v=")[1];
 
 const getActiveVideo = async () =>
 	waitForElement("#player #container.ytd-player");
 
-const renderOverlayOnVideo = (activeVideo: HTMLElement) => {
-	const overlay = document.createElement("div");
-	overlay.id = "overlay";
-	overlay.style.cssText = `
-    width: 100%;
-    height: 100%;
-    position: absolute;
-    top: 0;
-    left: 0;
-    background-color: rgba(0, 0, 0, 0.5);
-    z-index: 10;
+const renderIframeOnVideo = async (activeVideo: HTMLElement) => {
+	iframe.id = "iframe";
+	iframe.style.cssText = `
+      width: 100%;
+      height: 100%;
+      position: absolute;
+      top: 0;
+      left: 0;
+      right: 0;
+      z-index: 10;
+      border-radius: 12px;
   `;
-	overlay.style.pointerEvents = "none";
 
 	activeVideo.style.position = "relative";
-	activeVideo.appendChild(overlay);
 
-	return overlay;
+	const videoId = getVideoId(location.href);
+
+	iframe.setAttribute(
+		"src",
+		`https://www.youtube.com/embed/${videoId}?loop=1&autoplay=1`,
+	);
+	iframe.setAttribute("id", videoId);
+
+	await waitForElement("#player-container");
+
+	const moviePlayer = activeVideo.querySelector("#movie_player");
+
+	if (!moviePlayer) return;
+
+	const isVideoOrAdsPlaying = moviePlayer.classList.contains("playing-mode");
+
+	await waitFor(500);
+
+	if (isVideoOrAdsPlaying) {
+		// pause current video
+		(moviePlayer as HTMLElement).click();
+	}
+
+	await waitFor(500);
+
+	activeVideo.appendChild(iframe);
 };
 
 (async () => {
 	setTimeout(async () => {
 		const activeVideo = await getActiveVideo();
 		if (!activeVideo) return;
-		renderOverlayOnVideo(activeVideo);
-	}, 4000);
+		await renderIframeOnVideo(activeVideo);
+	}, 1000);
 })();
