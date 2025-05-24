93a282410f7c47c8c0698ff200c2b1e75be34987
Luqman Olushi <olushilukmon03@gmail.com>
2025-05-24 06:56:58 +0100

    feat: mute player from UI

--- src/youtube-player.ts
+++ src/youtube-player.ts
@@ -30,12 +30,37 @@ export class YoutubePlayer {
 		return !this.player.paused;
 	}
 
+	private getMuteButton() {
+		return this.playerContainer.querySelector("button.ytp-mute-button") as
+			| HTMLButtonElement
+			| undefined;
+	}
+
+	private isMutedFromUI() {
+		const muteButton = this.getMuteButton();
+
+		// If the mute button has the speaker icon, it's unmuted.
+		return !muteButton?.querySelector(".ytp-svg-volume-animation-speaker");
+	}
+
 	mute() {
 		this.player.muted = true;
+
+		const muteButton = this.getMuteButton();
+
+		if (muteButton && !this.isMutedFromUI()) {
+			muteButton.click(); // Click the mute button to mute.
+		}
 	}
 
 	unmute() {
 		this.player.muted = false;
+
+		const muteButton = this.getMuteButton();
+
+		if (muteButton && this.isMutedFromUI()) {
+			muteButton.click(); // Click the mute button to unmute.
+		}
 	}
 
 	mount(node: HTMLElement) {
